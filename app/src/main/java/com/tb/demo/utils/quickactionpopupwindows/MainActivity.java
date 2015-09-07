211e2c0838b7313bd585c634e3b45681e2422f1c
tangbin <tang123>
2015-09-07 23:21:58 +0800

    new project

--- app/src/main/java/com/tb/demo/utils/quickactionpopupwindows/MainActivity.java
+++ app/src/main/java/com/tb/demo/utils/quickactionpopupwindows/MainActivity.java
@@ -1,11 +1,9 @@
 package com.tb.demo.utils.quickactionpopupwindows;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
